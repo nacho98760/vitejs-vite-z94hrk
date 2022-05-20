e9de82d401a84ad005e8b89b4b6b74a4aa4c0c41
nacho98760 <nacho987600@gmail.com>
2022-05-20 18:04:32 -0300

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos de FreeCodeCamp</h1>
-      <Testimony />
+      <Testimony
+        nombre="Emma Bostian"
+        pais="Suecia"
+        cargo="Ingeniera de Software"
+        empresa="Spotify"
+        testimonio="Siempre he tenido problemas al aprender JavaScript. He tomado muchos
+        cursos, pero el curso de FreeCodeCamp fue el que se quedó. Estudiar
+        JavaScript, así como estructuras de datos y algoritmos en FreeCodeCamp
+        me dió las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software de Spotify."
+      />
+      <Testimony
+        nombre="Shawn Wang"
+        pais="Singapur"
+        cargo="Ingeniero de Sofware"
+        empresa="Amazon"
+        testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podia programar trabajando a través de los cientos de horas de lecciones gratuitas de freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambio mi vida"
+      />
+      <Testimony
+        nombre="Sarah Chima"
+        pais="Nigeria"
+        cargo="Ingeniera de Sofware"
+        empresa="ChatDesk"
+        testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrolladora de software. El plan de estudios bien estructurado llevó mis conocimientos de programacion de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble"
+      />
     </div>
     </div>
   );
--- src/testimony.jsx
+++ src/testimony.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './stylesheets/testimony.css';
 
-function Testimony() {
+function Testimony(props) {
   return (
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio" src=""/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">
-          "Siempre he tenido problemas al aprender JavaScript. He tomado muchos
-          cursos, pero el curso de FreeCodeCamp fue el que se quedó. Estudiar
-          JavaScript, así como estructuras de datos y algoritmos en FreeCodeCamp
-          me dió las habilidades y la confianza que necesitaba para conseguir el
-          trabajo de mis sueños como ingeniero de software de Spotify."
-        </p>
+        <p className="nombre-testimonio">{props.nombre} en <b>{props.pais}</b></p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
